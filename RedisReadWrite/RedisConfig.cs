839e7676c5b4b77b1446c2a83d3ee73ab3062122
JamesFoxChen <25717594@qq.com>
2015-10-21 18:17:57 +0800

    redis主从

---
+++ RedisReadWrite/RedisConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+
+namespace RedisReadWrite
+{
+    class RedisConfig
+    {
+        public static RedisClient Redis
+        {
+            get
+            {
+                return (RedisClient)reidsPools.GetClient();
+            }
+        }
+
+        private static string[] hosts;
+        private static PooledRedisClientManager reidsPools;
+        static RedisConfig()
+        {
+            var readWriteHosts = ConfigurationManager.AppSettings["ReadWriteHosts"].Split(',');
+            var readOnlyHosts = ConfigurationManager.AppSettings["ReadOnlyHosts"].Split(',');
+
+            reidsPools = new PooledRedisClientManager(readWriteHosts, readOnlyHosts, new RedisClientManagerConfig
+            {
+                MaxWritePoolSize = 100,//“写”链接池链接数
+                MaxReadPoolSize = 200,//“读”链接池链接数
+                AutoStart = true,
+                DefaultDb = 0
+            });
+        }
+    }
+}
